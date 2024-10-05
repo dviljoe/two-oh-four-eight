6d3f2e9e59af462114b6cdd4ce77adad90784364
DewaldV <dewald.viljoen@pm.me>
2024-10-05 12:13:21 +0100

    flake: upgrade rust

--- flake.nix
+++ flake.nix
@@ -1,29 +1,31 @@
 {
   inputs = {
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.follows = "rust-overlay/flake-utils";
     nixpkgs.follows = "rust-overlay/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        rust-stable = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-        };
-      in with pkgs; {
+      in
+      with pkgs;
+      {
         devShells.default = mkShell rec {
           nativeBuildInputs = [
-            semgrep
             pkg-config
-            rust-bin.stable.latest.clippy
-            rust-bin.stable.latest.rust-analyzer
-            rust-stable
+            rust-bin.stable.latest.complete
             clang
             mold
-            gnumake
 
           ];
           buildInputs = [
@@ -38,5 +40,6 @@
           ];
           LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
         };
-      });
+      }
+    );
 }
